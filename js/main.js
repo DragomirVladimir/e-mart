35c47b77cd052a6fb2fda4126eb96da3d501b672
DragomirVladimir <drag.v777@gmail.com>
2023-02-06 00:47:11 +0200

    added styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function () {
 
+  $('.main-screen__inner').slick({
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    dots: true,
+    centerMode: 0,
+    arrows: false,
+  })
 
 
 })
