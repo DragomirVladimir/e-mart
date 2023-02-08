a56c38c55ec14dd63f669fc453ecdb293e7f2ca9
DragomirVladimir <drag.v777@gmail.com>
2023-02-08 13:33:34 +0200

    added product card

--- js/main.js
+++ js/main.js
@@ -10,4 +10,14 @@ $(function () {
   })
 
 
+  const ratingItemsList = document.querySelectorAll('.rating__item');
+  const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+
+  ratingItemsArray.forEach(item =>
+    item.addEventListener('click', () =>
+      item.parentNode.dataset.totalValue = item.dataset.itemValue
+    )
+  );
+
+
 })
