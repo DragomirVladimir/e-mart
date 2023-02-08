0be0dae0416a790196527b072f2956ca9650819d
DragomirVladimir <drag.v777@gmail.com>
2023-02-08 23:32:17 +0200

    added nav

--- js/main.js
+++ js/main.js
@@ -14,8 +14,13 @@ $(function () {
   const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
 
   ratingItemsArray.forEach(item =>
-    item.addEventListener('click', () =>
-      item.parentNode.dataset.totalValue = item.dataset.itemValue
+    item.addEventListener('click', () => {
+      const { itemValue } = item.dataset;
+      item.parentNode.dataset.totalValue = itemValue;
+
+      //request
+    }
+
     )
   );
 
