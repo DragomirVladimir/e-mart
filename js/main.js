261fd6c74c882908c62e68e003868431492bc971
DragomirVladimir <drag.v777@gmail.com>
2023-02-24 23:05:19 +0200

    cart logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(function () {
 
+
+
   $('.nav__link').on('click', function () {
     $('.nav__link').removeClass('nav__link--active');
     $(this).addClass('nav__link--active');
@@ -73,56 +75,16 @@ $(function () {
       item.parentNode.dataset.totalValue = itemValue;
 
       //request
-    }
-
-    )
+    })
   );
 
-  //Работа с корзиной//
-
-  // const counterQnt = document.querySelector('.cart-qnt');
 
 
-  // $('.add-to-cart').on('click', function (event) {
-
-  //   counterQntValue = counterQntValue + 1;
-
-  //   counterQnt.innerHTML = counterQntValue;
-
-  //   // const productCard = event.target.closest('.product-card');
-
-
-  // }); 
-
-
-  let counterQntValue = 0;
-
-  $('[data-cart]').on('click', function (event) {
-
-    counterQntValue = ++counterQntValue;
-
-    $('.cart-qnt').html(counterQntValue);
-
-    // console.log(event.target.closest('.product-card'));
-
-    const product = event.target.closest('.product-card');
-
-    const productInfo = {
-      productId: product.dataset.productId,
-      imgSrc: product.querySelector('.product-card__img').getAttribute('src'),
-      imgAlt: product.querySelector('.product-card__img').getAttribute('alt'),
-      tetle: product.querySelector('.product-card__title').innerText,
-      price: product.querySelector('.product-card__price').innerText,
-    }
-
-    console.log(productInfo);
-
-  });
-
+  //Cart//
 
   $('.header__link--cart').on('click', function () {
     $('.cart').toggleClass('cart--open');
-    $('body').addClass('disable-scroll');
+    $('body').toggleClass('disable-scroll');
   })
 
   $('.cart__close-btn').on('click', function () {
@@ -131,6 +93,149 @@ $(function () {
   })
 
 
+  function reountCart() {
+
+    $('.cart-qnt').html($('[data-cart-list] article').length);
+
+  }
+
+  $('[data-cart]').on('click', function (event) {
+    event.preventDefault();
+
+    const product = $(this).closest('.product-card');
+
+    const productInfo = {
+      productId: product.data('product-id'),
+      imgSrc: $('.product-card__img', product).attr('src'),
+      imgAlt: $('.product-card__img', product).attr('alt'),
+      title: $('.product-card__title', product).text().trim(),
+      price: $('.product-card__price', product).text().trim(),
+    };
+
+
+    const cartProduct = `<article class="cart-product" data-id="${productInfo.productId}">
+  
+      <button class="cart-product__remove-btn" data-remove-product>
+        <span class="sr-only">remove from cart</span>
+        <span class="cart-product__remove-btn-icon"></span>
+      </button>
+    
+      <div class="cart-product__wrapper">
+        <div class="cart-product__main">
+          <a class="cart-product__img-link" href="#">
+            <img class="cart-product__img" src="${productInfo.imgSrc}" alt="${productInfo.imgAlt}">
+          </a>
+    
+          <div class="cart-product__info">
+            <a class="cart-product__title-link" href="#">
+              <h1 class="cart-product__title">
+                ${productInfo.title}
+              </h1>
+            </a>
+            <div class="cart-product__price">
+            ${productInfo.price}
+            </div>
+          </div>
+    
+        </div>
+    
+        <div class="cart-product__counter">
+    
+          <div class="cart-product__counter-box">
+            <button class="cart-product__counter-btn cart-product__counter-btn--minus" type="button"
+              data-counter-remove>
+            </button>
+            <span class="cart-product__counter-value">
+              1
+            </span>
+            <button class="cart-product__counter-btn cart-product__counter-btn--plus" type="button" data-counter-add>
+    
+            </button>
+          </div>
+    
+          <div class="cart-product__total-price">
+            £423.00
+          </div>
+    
+        </div>
+      </div>
+    
+    </article>`;
+
+
+    const currentCart = $(`[data-cart-list] [data-id="${productInfo.productId}"]`);
+
+    if (currentCart.length) {
+      const counterSelect = currentCart.find('.cart-product__counter-value');
+      const counterItems = counterSelect.text();
+
+      counterSelect.html(parseInt(counterItems) + 1);
+    } else {
+      $('[data-cart-list]').append(cartProduct);
+    }
+
+    reountCart();
+
+
+  });
+
+
+  $(document).on('click', '[data-counter-add]', function () {
+
+    const productCounter = $(this).closest('.cart-product__counter-box');
+    const counterValue = productCounter.find('.cart-product__counter-value');
+    const counterValueText = counterValue.text();
+    counterValue.html(parseInt(counterValueText) + 1);
+
+  })
+
+  $(document).on('click', '[data-counter-remove]', function () {
+
+    const productCounter = $(this).closest('.cart-product__counter-box');
+    const counterValue = productCounter.find('.cart-product__counter-value');
+    const counterValueText = counterValue.text();
+
+    if (counterValueText >= 2) {
+
+      counterValue.html(parseInt(counterValueText) - 1);
+
+    }
+
+  })
+
+
+  $(document).on('click', '[data-remove-product]', function () {
+
+    const cartProduct = $(this).closest('.cart-product');
+
+    cartProduct.remove()
+
+    reountCart();
+  })
+
+
+
+
+
   var mixer = mixitup('.products__list');
 
-})
+});
+
+// const allCartBtn = document.querySelectorAll('.add-to-cart');
+
+
+  // console.log(allCartBtn);
+
+  // allCartBtn.forEach((button, index) => {
+  //   button.addEventListener('click', () => {
+
+
+
+  //     console.log("forEach worked", index);
+
+
+
+
+
+  //   });
+  // });
